90baf365f758570fb4df78f5bf425ccfe758e1d9
GUJJULA_BHAVANA <bg0586@srmist.edu.in>
2025-09-07 16:35:34 +0530

    signup backend is completed with basic functionality

--- Backend/Routes/userRouter.js
+++ Backend/Routes/userRouter.js
@@ -1,21 +1,35 @@
 const {Router} = require("express");
 const { UserModel } = require("../db");
 
+console.log("Checking UserModel:", UserModel); 
+
 const UserRouter = Router();
 
 UserRouter.post("/signup",async function(req, res) {
-    const {email, password, firstName, lastName} = req.body;
 
-    await UserModel.create({
-        email,
-        password,               // the above line and this line does the same job
-        firstName,
-        lastName
-    })
+    console.log("Attempting to sign up user with data:", req.body);
+    try{
+        const {email, password, firstName, lastName} = req.body;
 
-    res.json({
-        message: "signed up successfully"
-    })
+        await UserModel.create({
+            email,
+            password,               // the above line and this line does the same job
+            firstName,
+            lastName
+        })
+
+        res.json({
+            message: "signed up successfully"
+        })
+    }
+    catch(error) {
+        console.log("signup failed");
+        console.log(error);
+
+        res.status(500).json({
+            message:"something went wrong. user was not created"
+        })
+    }
 })
 
 UserRouter.post("/signin", async function (req, res) {
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,16 @@
-const { UserModel } = require("./db");
 const express = require("express");
 const app = express();
 app.use(express.json());
 const mongoose = require("mongoose");
 const { UserRouter } = require("./Routes/userRouter");
 require('dotenv').config();
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));       //this is a middleware.used for parsing data from HTML forms
+
+const staticPath = path.resolve(__dirname, "../Frontend/User-Signup");
+console.log("Serving static files from:", staticPath);
+app.use(express.static(staticPath));
 
 app.use("/TerraceGarden/User", UserRouter);
 
